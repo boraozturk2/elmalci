6f1915067fddd20347a3e03f783554708ed6e90f
boraozturk@hotmail.com <boraozturk@hotmail.com>
2017-07-18 17:56:00 +0300

    gdfgdfg

--- src/main/java/com/bozturk/idle/dto/StoreDto.java
+++ src/main/java/com/bozturk/idle/dto/StoreDto.java
@@ -6,19 +6,87 @@ import com.bozturk.idle.model.address.Country;
 
 public class StoreDto {
 
-	private List<Country> countries;
-
-	public StoreDto() {
-		// TODO Auto-generated constructor stub
+	private Long storeId;
+	
+	private String storeName;
+	private String country;
+	private String city;
+	private String counry;
+	private String area;
+	private String neighborhood;
+	private String cadde;
+	private String sokak;
+	private String kapi;
+	private String daire;
+	public Long getStoreId() {
+		return storeId;
 	}
-
-	public List<Country> getCountries() {
-		return countries;
+	public void setStoreId(Long storeId) {
+		this.storeId = storeId;
 	}
-
-	public void setCountries(List<Country> countries) {
-		this.countries = countries;
+	public String getStoreName() {
+		return storeName;
 	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCounry() {
+		return counry;
+	}
+	public void setCounry(String counry) {
+		this.counry = counry;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+	public String getCadde() {
+		return cadde;
+	}
+	public void setCadde(String cadde) {
+		this.cadde = cadde;
+	}
+	public String getSokak() {
+		return sokak;
+	}
+	public void setSokak(String sokak) {
+		this.sokak = sokak;
+	}
+	public String getKapi() {
+		return kapi;
+	}
+	public void setKapi(String kapi) {
+		this.kapi = kapi;
+	}
+	public String getDaire() {
+		return daire;
+	}
+	public void setDaire(String daire) {
+		this.daire = daire;
+	}
+	
+	
+	
 
 	
 
